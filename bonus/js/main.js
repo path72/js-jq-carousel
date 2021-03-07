d4714c84e9e7cc2c4468e9a3558c49d4648b048b
Salvatore Trimarchi <path72@gmail.com>
2021-03-07 19:27:24 +0100

    no classe active di default in html

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -4,11 +4,17 @@
 $(document).ready(function() {
 // ********************* doc ready start ***
 
-// ** BUTTON INIT **
-btnInit();
 
-// ** NAV INIT ** 
-navInit();
+var firstActiveImagePos = 3;
+
+// ** KICK-OFF **
+
+// nav init
+navInit(firstActiveImagePos); 
+
+// prev/next button & arrow key init
+btnKeyInit();
+
 
 // *********************** doc ready end ***
 });
@@ -16,98 +22,105 @@ navInit();
 //###################################################### 
 // FUNCTIONS
 
-// ** INIT **
-function btnInit() {
-  $('.prev_btn').click(function() { btnClick('prev'); });
-  $('.next_btn').click(function() { btnClick('next'); });
-  $('body').keydown(function(_ev) {
-    if      (_ev.keyCode == 37) { btnClick('prev'); }
-    else if (_ev.keyCode == 39) { btnClick('next'); }
-  });
-  arrowVisualEffectInit();
+// *** INIT ***
+function navInit(_firstActiveImagePos) {
+	/**
+	 * nav deploy
+	 * <i class="fas fa-circle"></i> selected
+	 * <i class="far fa-circle"></i> unselected
+	 */
+	var fa0 = '<i class="far fa-circle"></i>', faSet = '';
+	for (var i=1; i<=getActiveImageOfASet().tot; i++) faSet += fa0;
+	$('.nav').html(faSet);
+	$('.fa-circle.fas').css( 'color', 'var(--purple)');
+	$('.fa-circle')
+		.css({ 'font-size':'1em', 'padding':'4px 3px', 'cursor':'pointer' })
+		.click(function(){ navClick($(this)); }); // $(this) is the clicked $('.fa-circle')
+	// first active image selection (after nav deploy)
+	imgUpdate(_firstActiveImagePos);
 }
-function navInit() {
-  var fa0 = '<i class="far fa-circle"></i>',
-      fa1 = '<i class="fas fa-circle"></i>',
-      faListHtml = '',
-      N = getActiveImageOfSet();
-  for (var i=1; i<=N[1]; i++) faListHtml += (i==N[0]) ? fa1 : fa0;
-  $('.nav').html(faListHtml);
-  $('.fa-circle.fas').css( 'color', 'var(--purple)');
-  $('.fa-circle')
-    .css({ 'font-size':'1em','padding':'3px 4px', 'cursor':'pointer' })
-    .click(function(){ navClick($(this)); });
+function btnKeyInit() {
+	$('.prev_btn').click(function() { btnKeyClick('prev'); });
+	$('.next_btn').click(function() { btnKeyClick('next'); });
+	$('body').keydown(function(_ev) {
+		if      (_ev.keyCode == 37) { btnKeyClick('prev'); }
+		else if (_ev.keyCode == 39) { btnKeyClick('next'); }
+	});
+	arrowVisualEffectInit();
 }
 function arrowVisualEffectInit() {
-  var pBtn = $('.prev_btn'),
-      nBtn = $('.next_btn'),
-      arrL = $('.fa-angle-left'),
-      arrR = $('.fa-angle-right'),
-      scaleUp   = { 'transition': '0.1s ease-in', 'transform': 'scale(1.2)' },
-      scaleDown = { 'transition': '0.1s ease-in', 'transform': 'scale(1.0)' };
-  pBtn.mouseenter(function() { arrL.addClass('highlight');    arrL.css(scaleUp);   });
-  pBtn.mouseleave(function() { arrL.removeClass('highlight'); arrL.css(scaleDown); });
-  nBtn.mouseenter(function() { arrR.addClass('highlight');    arrR.css(scaleUp);   });
-  nBtn.mouseleave(function() { arrR.removeClass('highlight'); arrR.css(scaleDown); });
+	var pBtn = $('.prev_btn'),
+		nBtn = $('.next_btn'),
+		pArr = $('.fa-angle-left'),
+		nArr = $('.fa-angle-right'),
+		scaleUp   = { 'transition': '0.1s ease-in', 'transform': 'scale(1.2)' },
+		scaleDown = { 'transition': '0.1s ease-in', 'transform': 'scale(1.0)' };
+	pBtn.mouseenter(function() { pArr.addClass('highlight');    pArr.css(scaleUp);   });
+	pBtn.mouseleave(function() { pArr.removeClass('highlight'); pArr.css(scaleDown); });
+	nBtn.mouseenter(function() { nArr.addClass('highlight');    nArr.css(scaleUp);   });
+	nBtn.mouseleave(function() { nArr.removeClass('highlight'); nArr.css(scaleDown); });
 }
 
-// ** CLICK ** 
-function btnClick(_shift) {
-  var N = getActiveImageOfSet(), newPos = null;
-  if      (N[0] == 1     && _shift == 'prev') newPos = N[1];
-  else if (N[0] == N[1]  && _shift == 'next') newPos = 1;
-  else    newPos = (_shift == 'next') ? (N[0]+1) : (N[0]-1);
-  imgUpdate(newPos);
-  navUpdate();
+// *** CLICK ACTIONS ***
+function btnKeyClick(_shift) {
+	var actPos = getActiveImageOfASet().pos,
+		totImg = getActiveImageOfASet().tot,
+		newPos = null;
+	if      (actPos == 1       && _shift == 'prev') newPos = totImg;
+	else if (actPos == totImg  && _shift == 'next') newPos = 1;
+	else    newPos = (_shift == 'next') ? (actPos + 1) : (actPos - 1);
+	imgUpdate(newPos);
 }
-function navClick(_navBtn) {
-  var navBtnSet = $('.nav > .fa-circle'), newPos = null;
-  for (var i=0; i<navBtnSet.length; i++) {
-    if (_navBtn[0] == navBtnSet[i]) newPos = i+1;
-  }
-  if (newPos != getActiveImageOfSet()[0]) imgUpdate(newPos);
+function navClick(_navBtn) { // _navBtn jQ object (clicked nav button)
+	var navBtnSet = $('.nav > .fa-circle'), // jQ object-array > navBtnSet[i]: single html structure 
+		newPos = null;
+	// clicked nav button > get position in set
+	for (var i=0; i<navBtnSet.length; i++) {
+		if (_navBtn[0] == navBtnSet[i]) newPos = i+1;
+	}
+	if (newPos != getActiveImageOfASet().pos) imgUpdate(newPos);
 }
 
-// ** VISUAL DYNAMICS ** 
+// *** VISUAL DYNAMICS ***
 function imgUpdate(_newPos) {
-  var N = getActiveImageOfSet();
-  for (var i=1; i<=N[1]; i++) {
-    var img = $('.img_container > img:nth-child('+i+')');
-    if (i == N[0]) {
-      img.removeClass('active');
-    } else if (i == _newPos) {
-      img.addClass('active');
-    }
-  }
-  navUpdate();
+	var actPosTot = getActiveImageOfASet();
+	for (var i=1; i<=actPosTot.tot; i++) {
+		var img = $('.img_container > img:nth-child('+i+')'); // jQ obcject
+		if (i == actPosTot.pos) {
+			img.removeClass('active');
+		} else if (i == _newPos) {
+			img.addClass('active');
+		}
+	}
+	navUpdate();
 }
 function navUpdate() {
-  var N = getActiveImageOfSet();  
-  for (var i=1; i<=N[1]; i++) {
-    var fa = $('.nav > .fa-circle:nth-child('+i+')');
-    if (i == N[0]) {
-      fa.addClass('fas');
-      fa.removeClass('far');
-      fa.css( 'color', 'var(--purple)');
-    } else {
-      fa.addClass('far');
-      fa.removeClass('fas');
-      fa.css( 'color', 'var(--black)');
-    }
-  }
+	var actPosTot = getActiveImageOfASet();  
+	for (var i=1; i<=actPosTot.tot; i++) {
+		var fa = $('.nav > .fa-circle:nth-child('+i+')'); // jQ obcject
+		if (i == actPosTot.pos) {
+			fa.addClass('fas');
+			fa.removeClass('far');
+			fa.css( 'color', 'var(--purple)');
+		} else {
+			fa.addClass('far');
+			fa.removeClass('fas');
+			fa.css( 'color', 'var(--black)');
+		}
+	}
 }
 
-// ** KEY TOOL **
-function getActiveImageOfSet() {
-  /**
-   * N[0] active image position
-   * N[1] total number of images
-   */
-  var imgSet = $('.img_container > img');
-  var N = [null,imgSet.length];
-  for (var i=1; i<=N[1]; i++) {
-    var img = $('.img_container > img:nth-child('+i+')');
-    if (img.hasClass('active')) N[0] = i;
-  }
-  return N;
+// *** KEY TOOL ***
+function getActiveImageOfASet() {
+	/**
+	 * .pos: active image position in a set
+	 * .tot: total number of images in the set
+	 */
+	var imgSet = $('.img_container > img'), // jQ object-array > imgSet[i]: single html structure 
+		obj = { 'pos': null,'tot': imgSet.length };
+	for (var i=1; i<=obj.tot; i++) {
+		var img = $('.img_container > img:nth-child('+i+')'); // jQ object
+		if (img.hasClass('active')) obj.pos = i;
+	}
+	return obj;
 }
